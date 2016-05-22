cd331621d0d1962ff5d089a2d5286a766fca6c2d
Max Glenister <moglenstar@gmail.com>
2016-05-22 10:53:44 +0100

    Added first bits of konami code to zoom to screen

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 (function() {
   'use strict';
-
-
+  cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+    // Zoom to screen
+    window.scrollTo(0, 0);
+    var konami_elemnts = document.querySelectorAll('.konami');
+    for (var i=0; i<konami_elemnts.length;i++) {
+      konami_elemnts[i].emit('konami');
+    }
+  });
 
 }());
